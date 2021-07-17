a336ab35b76136ac25c0d5b68bae881afc54b6b3
Ruitao Wang <wang.ruitao@outlook.com>
2021-07-17 02:35:29 +0000

    名字修正

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ with open("README.md", "rb") as fh:
 
 
 setuptools.setup(
-    # name="datarepo",
-    # version="1.0",
-    # author="Ruitao Wang",
-    # author_email="wang.ruitao@outlook.com",
-    # description="A Framework for automatic data updating",
+    name="datarepo",
+    version="1.0",
+    author="Ruitao Wang",
+    author_email="wang.ruitao@outlook.com",
+    description="A Framework for automatic data updating",
     # long_description=long_description,
     # long_description_content_type="text/markdown",
     # packages=setuptools.find_packages(
     #     exclude=["*.git", "__pycache__/", ".vscode/"]
     # ),
-    # python_requires=">=3",
-    # install_requires=[
-    #     "numpy",
-    #     "pandas",
-    #     "sqlalchemy"
-    # ]
+    python_requires=">=3",
+    install_requires=[
+         "numpy",
+         "pandas",
+         "sqlalchemy"
+     ]
 )
